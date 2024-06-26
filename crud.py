78c658e9b2c2a876a0d8997d64e59af548710880
Alexander Kisliy <108825919+SashaKisliy@users.noreply.github.com>
2024-06-26 12:53:34 +0300

    Solution

---
+++ crud.py
@@ -0,0 +1,47 @@
+from sqlalchemy.orm import Session
+from models import Author, Book
+from schemas import AuthorCreate, BookCreate
+
+
+def get_author_by_name(db: Session, name: str) -> Author:
+    return db.query(Author).filter(Author.name == name).first()
+
+
+def get_all_authors(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(Author).offset(skip).limit(limit).all()
+
+
+def get_author_by_id(db: Session, id: int) -> Author:
+    return db.query(Author).filter(Author.id == id).first()
+
+
+def create_author(db: Session, author: AuthorCreate):
+    db_author = Author(
+        name=author.name,
+        bio=author.bio,
+    )
+    db.add(db_author)
+    db.commit()
+    db.refresh(db_author)
+    return db_author
+
+
+def get_all_books(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(Book).offset(skip).limit(limit).all()
+
+
+def get_books_by_author_id(db: Session, author_id: int, skip: int = 0, limit: int = 10):
+    return db.query(Book).filter(Book.author_id == author_id).offset(skip).limit(limit).all()
+
+
+def create_book(db: Session, book: BookCreate):
+    db_book = Book(
+        title=book.title,
+        summary=book.summary,
+        publication_date=book.publication_date,
+        author_id=book.author_id,
+    )
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+    return db_book
---
+++ main.py
@@ -0,0 +1,60 @@
+from typing import List
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+import crud
+from database import SessionLocal
+from schemas import AuthorList, AuthorCreate, BookList, BookCreate
+
+app = FastAPI()
+
+
+def get_db() -> Session:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/authors/", response_model=List[AuthorList])
+def read_authors(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    return crud.get_all_authors(db=db, skip=skip, limit=limit)
+
+
+@app.get("/authors/{author_id}", response_model=AuthorList)
+def read_author_by_id(author_id: int, db: Session = Depends(get_db)):
+    author = crud.get_author_by_id(db=db, id=author_id)
+    if author is None:
+        raise HTTPException(status_code=404, detail="Author not found")
+    return author
+
+
+@app.post("/authors/", response_model=AuthorList)
+def create_author(author: AuthorCreate, db: Session = Depends(get_db)):
+    db_author = crud.get_author_by_name(db=db, name=author.name)
+    if db_author:
+        raise HTTPException(status_code=400, detail="Author already exists")
+    return crud.create_author(db=db, author=author)
+
+
+@app.get("/books/", response_model=List[BookList])
+def read_books(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    return crud.get_all_books(db=db, skip=skip, limit=limit)
+
+
+@app.get("/books/author/{author_id}", response_model=List[BookList])
+def read_books_by_author(author_id: int, skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    books = crud.get_books_by_author_id(db, author_id=author_id, skip=skip, limit=limit)
+    if not books:
+        raise HTTPException(status_code=404, detail="Books not found for this author")
+    return books
+
+
+@app.post("/books/", response_model=BookList)
+def create_book(book: BookCreate, db: Session = Depends(get_db)):
+    return crud.create_book(db=db, book=book)
